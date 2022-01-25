c02eef65bc89188439d30253128e33982e27ced5
haydenridd98 <haydenridd98@gmail.com>
2022-01-24 17:09:13 -0700

    Add project files.

---
+++ Mission4/Migrations/20220125000611_Initial.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission4.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "responses",
+                columns: table => new
+                {
+                    ApplicationId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Category = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    Director = table.Column<string>(nullable: false),
+                    Rating = table.Column<string>(nullable: false),
+                    Edited = table.Column<bool>(nullable: false),
+                    LentTo = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_responses", x => x.ApplicationId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "ApplicationId", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 1, "Drama", "Richard Curtis", false, "", "", "R", "About Time", 2013 });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "ApplicationId", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 2, "SciFi", "Christopher Nolan", false, "", "", "PG-13", "Interstellar", 2014 });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "ApplicationId", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 3, "Action/Adventure", "Christopher Nolan", false, "", "", "PG-13", "The Dark Knight", 2008 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "responses");
+        }
+    }
+}
---
+++ Mission4/Models/MovieContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Models
+{
+    public class MovieContext : DbContext
+    {
+        // constructor
+        public MovieContext (DbContextOptions<MovieContext> options) : base (options)
+        {
+            // blank for now
+        }
+
+        public DbSet<CreateMovie> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<CreateMovie>().HasData(
+                
+                new CreateMovie
+                {
+                    ApplicationId = 1,
+                    Category = "Drama",
+                    Title = "About Time",
+                    Year = 2013,
+                    Director = "Richard Curtis",
+                    Rating = "R",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+
+                new CreateMovie
+                {
+                    ApplicationId = 2,
+                    Category = "SciFi",
+                    Title = "Interstellar",
+                    Year = 2014,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+
+                new CreateMovie
+                {
+                    ApplicationId = 3,
+                    Category = "Action/Adventure",
+                    Title = "The Dark Knight",
+                    Year = 2008,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                }
+            );
+        }
+    }
+}
